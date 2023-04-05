7b66669756c7ee35021296b6856122036563f8b3
Diego Franco Giusti Modesto da Silva <diegogiusti08@gmail.com>
2023-04-04 22:40:11 -0300

    refatorando o código e adicionando o modal

--- app.js
+++ app.js
@@ -1,17 +1,26 @@
 const form = document.querySelector('.quiz-form')
-
+const modal = document.querySelector('.modal')
+const totalPoints = document.querySelector('.points')
 const correctAnwers = ['C','B','B','A']
+let score = 0
 
-form.addEventListener('submit', event => {
-    event.preventDefault()
+const validatingAnwers = (userAnwer,index) => {
+    const isCorrectAnwer = userAnwer.value === correctAnwers[index]
 
+    if(isCorrectAnwer){
+        score += 25
+    }
+}
+
+const showScore =  event => {
+    event.preventDefault()  
     const userAnwers = [event.target.inputQuestion1,event.target.inputQuestion2,event.target.inputQuestion3,event.target.inputQuestion4]
-    let score = 0
 
-    userAnwers.forEach((userAnwer,index) => {
-        if(userAnwer.value === correctAnwers[index]){
-            score += 25
-        }
-    })
+    userAnwers.forEach(validatingAnwers)
     console.log(score)
-})
+    modal.style.display = 'block'
+    totalPoints.textContent = score
+    score = 0
+}
+
+form.addEventListener('submit', showScore)
