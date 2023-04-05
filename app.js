69022b05a1ed913bcd971091dedd92b550034bbb
Diego Franco Giusti Modesto da Silva <diegogiusti08@gmail.com>
2023-04-04 21:22:47 -0300

    adicionando a lógica do quiz

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
+const form = document.querySelector('.quiz-form')
+
 const correctAnwers = ['C','B','B','A']
+
+form.addEventListener('submit', event => {
+    event.preventDefault()
+
+    const userAnwers = [event.target.inputQuestion1,event.target.inputQuestion2,event.target.inputQuestion3,event.target.inputQuestion4]
+    let score = 0
+
+    userAnwers.forEach((userAnwer,index) => {
+        if(userAnwer.value === correctAnwers[index]){
+            score += 25
+        }
+    })
+    console.log(score)
+})
